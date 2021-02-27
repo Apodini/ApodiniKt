002cc7c29c40737f10df785731144e96b762dc95
Mathias Quintero <me@quintero.io>
2021-02-27 22:54:47 +0100

    Testing with path parameter

--- src/test/kotlin/de/tum/in/ase/apodini/test/TestWebService.kt
+++ src/test/kotlin/de/tum/in/ase/apodini/test/TestWebService.kt
@@ -5,11 +5,15 @@ import de.tum.`in`.ase.apodini.Handler
 import de.tum.`in`.ase.apodini.WebService
 import de.tum.`in`.ase.apodini.impl.text
 import de.tum.`in`.ase.apodini.impl.group
+import de.tum.`in`.ase.apodini.properties.PathParameter
 import de.tum.`in`.ase.apodini.properties.options.http
 import de.tum.`in`.ase.apodini.properties.parameter
+import de.tum.`in`.ase.apodini.properties.pathParameter
 import kotlin.coroutines.CoroutineContext
 
 class TestWebService : WebService {
+    private val id = pathParameter<String>()
+
     override fun ComponentBuilder.invoke() {
         text("Hello World")
         group("kotlin") {
@@ -19,9 +23,21 @@ class TestWebService : WebService {
             }
         }
 
+        group("user", id) {
+            GreeterForUser(id)
+        }
+
         group("greeting") {
             +Greeter()
         }
+    }
+}
+
+class GreeterForUser(id: PathParameter<String>) : Handler<String> {
+    private val id by id
+
+    override suspend fun CoroutineContext.compute(): String {
+        return "Hello, User $id"
     }
 }
 
