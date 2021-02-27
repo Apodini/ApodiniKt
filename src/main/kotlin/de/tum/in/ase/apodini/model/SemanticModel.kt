2dc579d1505e8b188699b0576ed228ddcf297465
Mathias Quintero <me@quintero.io>
2021-02-27 22:54:39 +0100

    Adding path component class to endpoint

--- src/main/kotlin/de/tum/in/ase/apodini/model/SemanticModel.kt
+++ src/main/kotlin/de/tum/in/ase/apodini/model/SemanticModel.kt
@@ -1,15 +1,24 @@
 package de.tum.`in`.ase.apodini.model
 
 import de.tum.`in`.ase.apodini.Handler
+import de.tum.`in`.ase.apodini.properties.Parameter
+import de.tum.`in`.ase.apodini.properties.PathParameter
 import de.tum.`in`.ase.apodini.types.TypeDefinition
 
 class SemanticModel internal constructor(
         val endpoints: List<Endpoint>
 ) {
+    sealed class PathComponent {
+        data class StringPathComponent(val value: String) : PathComponent()
+        data class ParameterPathComponent(val parameter: PathParameter<*>) : PathComponent()
+    }
+
     abstract class Endpoint
 
     class ConcreteEndpoint<T>(
+            val path: Iterable<PathComponent>,
             val typeDefinition: TypeDefinition<T>,
-            val handler: Handler<T>
+            val handler: Handler<T>,
+            val parameters: Iterable<Parameter<*>>,
     ) : Endpoint()
 }
