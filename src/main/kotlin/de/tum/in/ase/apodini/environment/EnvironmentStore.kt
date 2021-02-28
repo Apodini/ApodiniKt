bc78b7bcad6a5eaa6e84be5585ac27dcbca6716f
Mathias Quintero <me@quintero.io>
2021-02-28 12:38:30 +0100

    Adding basic environment store implementation

---
+++ src/main/kotlin/de/tum/in/ase/apodini/environment/EnvironmentStore.kt
@@ -0,0 +1,46 @@
+package de.tum.`in`.ase.apodini.environment
+
+interface EnvironmentStore : EnvironmentKeys {
+    fun contains(key: EnvironmentKey<*>): Boolean
+
+    operator fun <T> get(key: EnvironmentKey<T>) : T
+
+    operator fun <T> EnvironmentKey<T>.invoke(): T {
+        return this@EnvironmentStore[this]
+    }
+
+    companion object {
+        val empty: EnvironmentStore = StandardEnvironmentStore(emptyMap())
+
+        operator fun invoke(init: EnvironmentBuilder.() -> Unit): EnvironmentStore {
+            return StandardEnvironmentStoreBuilder().also(init).build()
+        }
+    }
+}
+
+private class StandardEnvironmentStore(
+        private val store: Map<EnvironmentKey<*>, Any?>
+) : EnvironmentStore {
+    override fun contains(key: EnvironmentKey<*>): Boolean {
+        return store.containsKey(key)
+    }
+
+    override operator fun <T> get(key: EnvironmentKey<T>) : T {
+        return store[key]?.let { value ->
+            @Suppress("UNCHECKED_CAST")
+            value as T
+        } ?: key.default
+    }
+}
+
+private class StandardEnvironmentStoreBuilder : EnvironmentBuilder {
+    private val map = mutableMapOf<EnvironmentKey<*>, Any?>()
+
+    override fun <T> EnvironmentKey<T>.invoke(value: () -> T) {
+        map[this] = value()
+    }
+
+    fun build(): StandardEnvironmentStore {
+        return StandardEnvironmentStore(map)
+    }
+}
