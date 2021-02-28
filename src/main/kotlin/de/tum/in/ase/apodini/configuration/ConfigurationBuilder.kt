8aaf6f8ee90cdecf23fb041aeeb94a053042eacf
Mathias Quintero <me@quintero.io>
2021-02-28 12:39:11 +0100

    Making configuration builder use environment builder

--- src/main/kotlin/de/tum/in/ase/apodini/configuration/ConfigurationBuilder.kt
+++ src/main/kotlin/de/tum/in/ase/apodini/configuration/ConfigurationBuilder.kt
@@ -1,15 +1,9 @@
 package de.tum.`in`.ase.apodini.configuration
 
-import de.tum.`in`.ase.apodini.environment.EnvironmentKey
-import de.tum.`in`.ase.apodini.environment.EnvironmentKeys
+import de.tum.`in`.ase.apodini.environment.EnvironmentBuilder
 import de.tum.`in`.ase.apodini.exporter.Exporter
-import kotlin.reflect.KProperty1
 
 interface ConfigurationBuilder {
     fun use(exporter: Exporter)
-    fun <T> environment(key: EnvironmentKey<T>, value: T)
-
-    fun <T> environment(key: KProperty1<EnvironmentKeys, EnvironmentKey<T>>, value: T) {
-        environment(key.get(EnvironmentKeys), value)
-    }
+    fun environment(init: EnvironmentBuilder.() -> Unit)
 }
