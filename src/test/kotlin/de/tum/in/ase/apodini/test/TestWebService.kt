364e7b92cd93e02a4dffa2a5d7cdc3e34aafe263
Mathias Quintero <me@quintero.io>
2021-02-28 13:17:04 +0100

    Using logging and authentication in web service

--- src/test/kotlin/de/tum/in/ase/apodini/test/TestWebService.kt
+++ src/test/kotlin/de/tum/in/ase/apodini/test/TestWebService.kt
@@ -3,12 +3,15 @@ package de.tum.`in`.ase.apodini.test
 import de.tum.`in`.ase.apodini.ComponentBuilder
 import de.tum.`in`.ase.apodini.Handler
 import de.tum.`in`.ase.apodini.WebService
+import de.tum.`in`.ase.apodini.configuration.ConfigurationBuilder
+import de.tum.`in`.ase.apodini.environment.request
+import de.tum.`in`.ase.apodini.exporter.RESTExporter
 import de.tum.`in`.ase.apodini.impl.text
 import de.tum.`in`.ase.apodini.impl.group
-import de.tum.`in`.ase.apodini.properties.PathParameter
+import de.tum.`in`.ase.apodini.logging.logger
+import de.tum.`in`.ase.apodini.properties.*
 import de.tum.`in`.ase.apodini.properties.options.http
-import de.tum.`in`.ase.apodini.properties.parameter
-import de.tum.`in`.ase.apodini.properties.pathParameter
+import java.lang.IllegalArgumentException
 import kotlin.coroutines.CoroutineContext
 
 object TestWebService : WebService {
@@ -23,11 +26,15 @@ object TestWebService : WebService {
             }
         }
 
+        group("me") {
+            +CurrentlyAuthenticatedUser()
+        }
+
         group("user", userId) {
-            GreeterForUser(userId)
+            +GreeterForUser(userId)
 
             group("post") {
-                PostsForUser(userId)
+                +PostsForUser(userId)
             }
         }
 
@@ -35,13 +42,52 @@ object TestWebService : WebService {
             +Greeter()
         }
     }
+
+    override fun ConfigurationBuilder.configure() {
+        use(RESTExporter())
+    }
+}
+
+data class User(val name: String, val age: Int) {
+    companion object : BasicAuthenticationUserFactory<User> {
+        override suspend fun user(username: String, password: String): User {
+            if (username == "test@example.org" && password == "password") {
+                return User("Mathias", 25)
+            }
+
+            throw IllegalArgumentException("Wrong Username and Password")
+        }
+
+    }
+}
+
+class CurrentlyAuthenticatedUser : Handler<User?> {
+    private val authenticated by authenticated(User)
+    private val logger by environment { logger }
+
+    override suspend fun CoroutineContext.compute(): User? {
+        if (authenticated == null) {
+            logger.debug("User is not authenticated")
+        }
+        return authenticated
+    }
 }
 
 class GreeterForUser(id: PathParameter) : Handler<String> {
     private val id by id
+    private val name by parameter<String?>()
+
+    private val request by environment { request }
+    private val logger by environment { logger }
 
     override suspend fun CoroutineContext.compute(): String {
-        return "Hello, User $id"
+        logger.info("Trying out logging")
+        logger.debug {
+            "Received id $id from $request"
+        }
+
+        val name = name ?: return "Hello, User $id"
+        return "Hello, $name (User $id)"
     }
 }
 
