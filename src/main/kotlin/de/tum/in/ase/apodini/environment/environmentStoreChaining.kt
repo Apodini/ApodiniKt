3a92d32fa1f89989e95592904a8ac25bab9f5437
Mathias Quintero <me@quintero.io>
2021-02-28 12:38:55 +0100

    Allowing to override or extend an environment store

---
+++ src/main/kotlin/de/tum/in/ase/apodini/environment/environmentStoreChaining.kt
@@ -0,0 +1,36 @@
+package de.tum.`in`.ase.apodini.environment
+
+fun EnvironmentStore.override(store: EnvironmentStore): EnvironmentStore {
+    return ChainedEnvironmentStore(default = store, fallback = this)
+}
+
+fun EnvironmentStore.override(init: EnvironmentBuilder.() -> Unit): EnvironmentStore {
+    return override(EnvironmentStore(init))
+}
+
+fun EnvironmentStore.extend(store: EnvironmentStore): EnvironmentStore {
+    return ChainedEnvironmentStore(default = this, fallback = store)
+}
+
+fun EnvironmentStore.extend(init: EnvironmentBuilder.() -> Unit): EnvironmentStore {
+    return extend(EnvironmentStore(init))
+}
+
+private class ChainedEnvironmentStore(
+        val default: EnvironmentStore,
+        val fallback: EnvironmentStore
+): EnvironmentStore {
+
+    override fun contains(key: EnvironmentKey<*>): Boolean {
+        return default.contains(key) || fallback.contains(key)
+    }
+
+    override fun <T> get(key: EnvironmentKey<T>): T {
+        if (default.contains(key)) {
+            return default[key]
+        }
+
+        return fallback[key]
+    }
+
+}
