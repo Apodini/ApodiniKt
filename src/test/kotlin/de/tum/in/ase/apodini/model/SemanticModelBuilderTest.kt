413e3d0640936137843a31e048ffd9128300b853
Mathias Quintero <me@quintero.io>
2021-03-06 19:06:59 +0100

    Adding more complex tests for semantic model

--- src/test/kotlin/de/tum/in/ase/apodini/model/SemanticModelBuilderTest.kt
+++ src/test/kotlin/de/tum/in/ase/apodini/model/SemanticModelBuilderTest.kt
@@ -1,13 +1,51 @@
 package de.tum.`in`.ase.apodini.model
 
-import de.tum.`in`.ase.apodini.test.TestWebService
+import de.tum.`in`.ase.apodini.ComponentBuilder
+import de.tum.`in`.ase.apodini.Handler
+import de.tum.`in`.ase.apodini.WebService
+import de.tum.`in`.ase.apodini.impl.text
+import de.tum.`in`.ase.apodini.types.Nullable
+import de.tum.`in`.ase.apodini.types.StringType
 import junit.framework.TestCase
+import kotlin.coroutines.CoroutineContext
 
 internal class SemanticModelBuilderTest : TestCase() {
 
-    fun testSemanticModel() {
-        val semanticModel = TestWebService.semanticModel()
-        print(semanticModel)
+    fun testHelloWorld() {
+        val semanticModel = semanticModel {
+            text("Hello World")
+        }
+        assertEquals(semanticModel.endpoints.count(), 1)
+
+        val endpoint = semanticModel.endpoints.first() as SemanticModel.ConcreteEndpoint<*>
+        assertEquals(endpoint.path.count(), 0)
+        assertEquals(endpoint.parameters.count(), 0)
+        assertEquals(endpoint.typeDefinition, StringType)
     }
 
+    fun testCustomHandlerWithOptionalString() {
+        val semanticModel = semanticModel {
+            +object : Handler<String?> {
+                override suspend fun CoroutineContext.compute(): String? {
+                    return null
+                }
+            }
+        }
+        assertEquals(semanticModel.endpoints.count(), 1)
+
+        val endpoint = semanticModel.endpoints.first() as SemanticModel.ConcreteEndpoint<*>
+        assertEquals(endpoint.path.count(), 0)
+        assertEquals(endpoint.parameters.count(), 0)
+        assertEquals(endpoint.typeDefinition, Nullable(StringType))
+    }
+
+    private fun semanticModel(init: ComponentBuilder.() -> Unit): SemanticModel {
+        return WrapperService(init).semanticModel()
+    }
+}
+
+private class WrapperService(private val init: ComponentBuilder.() -> Unit) : WebService {
+    override fun ComponentBuilder.invoke() {
+        init()
+    }
 }
